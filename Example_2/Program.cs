ddd96a93cc27ac17937acca79be25c1986fd0b24
Victoria <victoriaerygina@yandex.ru>
2022-08-23 16:20:13 +0700

    Задача 2

---
+++ Example_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+
+int number = new Random().Next(100, 1000);
+
+int result = ((number / 100) * 10) + (number % 10);
+
+Console.WriteLine($"Number = {number}. Result = {result}");
+
