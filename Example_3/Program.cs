75fb1f49d050f6a4cf017359502989fb7d1ba978
Victoria <victoriaerygina@yandex.ru>
2022-08-23 16:50:54 +0700

    Задача 3 (кратность чисел)

---
+++ Example_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number2 > Number1)
+{
+    if (Number2 % Number1 == 0)
+    {
+        Console.WriteLine($"Число {Number2} кратно числу {Number1}");
+    }
+    else
+    {
+        int result = Number2 % Number1;
+        Console.WriteLine($"Число {Number2} НЕ кратно числу {Number1}, остаток от деления = {result}");
+    }
+}
+else
+{
+    Console.WriteLine($"{Number2} < {Number1}");
+}
